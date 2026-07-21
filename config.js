feddb96013ae660db0444289a98ec3547438026c
fcarrascomarin <fcarrascomarin95@gmail.com>
2026-07-21 18:26:59 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-// CM Banquetería · web pública estática en GitHub Pages.
-// Backend/panel interno en Render:
 window.CM_API_BASE = 'https://cmbanqueteria.onrender.com';
 window.CM_ADMIN_URL = 'https://cmbanqueteria.onrender.com/admin.html';
